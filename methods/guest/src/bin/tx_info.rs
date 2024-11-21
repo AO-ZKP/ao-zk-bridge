a0f677426149ed7e966694ed81467750401ced31
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-11-22 04:57:16 +0530

    fix guest method, tx_info, assert var

--- methods/guest/src/bin/tx_info.rs
+++ methods/guest/src/bin/tx_info.rs
@@ -46,7 +46,7 @@ fn main() {
         .call();
 
     // Check that the given account holds at least 1 token.
-    assert!(returns.amount >= U256::from(1));
+    assert!(result.amount >= U256::from(1));
 
     // Create and commit journal with flattened fields
     let journal = Journal {
