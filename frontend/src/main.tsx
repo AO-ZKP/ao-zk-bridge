adc6047aed2ad0cb06070de8e81c42840c966f8b
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-11-25 18:40:51 +0530

    frontend ready

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ArweaveWalletKit } from "arweave-wallet-kit";
 import App from "./App.tsx";
 import "@/styles/globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ArweaveWalletKit
+      config={{
+        permissions: ["ACCESS_ADDRESS", "SIGN_TRANSACTION"],
+        ensurePermissions: true,
+      }}
+    >
+      <App />
+    </ArweaveWalletKit>
   </StrictMode>
 );
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,19 @@
-/// <reference types="vite/client" />
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ArweaveWalletKit } from "arweave-wallet-kit";
+import App from "./App.tsx";
+import "@/styles/globals.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ArweaveWalletKit
+      config={{
+        permissions: ["ACCESS_ADDRESS", "SIGN_TRANSACTION"],
+        ensurePermissions: true,
+      }}
+    >
+      <App />
+    </ArweaveWalletKit>
+  </StrictMode>
+);
+
