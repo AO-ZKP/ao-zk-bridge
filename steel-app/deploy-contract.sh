ae1410c676a184720a13df6f489b5938615af6df
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-12-30 04:34:48 +0530

    fix contract name in command

--- steel-app/deploy-contract.sh
+++ steel-app/deploy-contract.sh
@@ -11,4 +11,4 @@ forge script --rpc-url $ETH_RPC_URL \
 
 export RECEIVER_ADDRESS=$(jq -r '.transactions[] | select(.contractName == "ETHReceiver") | .contractAddress' broadcast/DeployReceiver.s.sol/${CHAIN_ID}/run-latest.json)
 
-forge verify-contract --chain-id $CHAIN_ID $RECEIVER_ADDRESS DeployReceiver --verifier-url $VERIFIER_URL_BLOCKSCOUT --verifier blockscout
+forge verify-contract --chain-id $CHAIN_ID $RECEIVER_ADDRESS ETHReceiver --verifier-url $VERIFIER_URL_BLOCKSCOUT --verifier blockscout
