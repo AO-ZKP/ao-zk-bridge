4eb9b5f6e2224253b7d431cb62f3f8498176d1c0
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-12-30 04:32:39 +0530

    add verification step in deployer

--- steel-app/deploy-contract.sh
+++ steel-app/deploy-contract.sh
@@ -5,4 +5,10 @@ source .env
 forge script --rpc-url $ETH_RPC_URL \
     --private-key $ETH_WALLET_PRIVATE_KEY \
     --broadcast \
+    --legacy \
+    --skip-simulation \
     DeployReceiver
+
+export RECEIVER_ADDRESS=$(jq -r '.transactions[] | select(.contractName == "ETHReceiver") | .contractAddress' broadcast/DeployReceiver.s.sol/${CHAIN_ID}/run-latest.json)
+
+forge verify-contract --chain-id $CHAIN_ID $RECEIVER_ADDRESS DeployReceiver --verifier-url $VERIFIER_URL_BLOCKSCOUT --verifier blockscout
